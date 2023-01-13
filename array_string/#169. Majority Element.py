95fcd2abb639f4c48d4f6b0715aed9328f068c5e
Jason Lee <changjin9792@gmail.com>
2023-01-13 23:09:27 +0900

    2023/1/13 Array 2, Hash 1, BFS 1

--- array_string/#169. Majority Element.py
+++ array_string/#169. Majority Element.py
@@ -1,15 +1,49 @@
 '''
+Method1
+TC: O(n)
+SC: O(1)
+'''
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt = 1
+        target = nums[0]
+
+        for e in nums[1:]:
+            if cnt == 0:
+                cnt = 1
+                target = e
+                continue
+            if target == e:
+                cnt += 1
+            else:
+                cnt -= 1
+
+        return target
+
+'''Method2
 TC: O(n)
 SC: O(n)
 '''
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        target_freq = n // 2
-        freq = defaultdict(int)
-        for e in nums:
-            freq[e] += 1
-            if freq[e] > target_freq:
-                return e
+        running_freq = 1
+        target = nums[0]
 
-        return None # guaranteed answer exists
+        for e in nums[1:]:
+            if e == target:
+                if running_freq == 0:
+                    target = e
+                running_freq += self.sign(running_freq)
+            else:
+                if running_freq == 0:
+                    target = e
+                running_freq += (-1 * self.sign(running_freq))
+
+        return target
+
+    def sign(self, i):
+        if i > 0:
+            return 1
+        else:
+            return -1
